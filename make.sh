1762940b9eef076dd20cfab149c9f97975d4b7d4
Jiang Bian <jbian@uams.edu>
2012-12-29 19:06:54 -0600

    typo ...

--- make.sh
+++ make.sh
@@ -6,6 +6,6 @@ PHP_BIN=$(which php)
 $PHP_BIN $ROOT/original/crypto-js-read-only/builder/builder.php
 cp -f $ROOT/original/crypto-js-read-only/build/rollups/crypto-js.js $ROOT/lib/crypto-js.js
 
-echo "if (typeof module !== 'undefined' && module.exports) {module.exports = CrytoJS;}" >> $ROOT/lib/crypto-js.js
+echo "if (typeof module !== 'undefined' && module.exports) {module.exports = CryptoJS;}" >> $ROOT/lib/crypto-js.js
 
 echo "COMPLETED!"
